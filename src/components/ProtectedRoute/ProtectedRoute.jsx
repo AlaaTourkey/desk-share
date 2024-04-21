90c330743869ebc58d89f843a928cf432234620f
Alaafathalla <alaafathalla36@gmail.com>
2024-04-21 23:55:23 +0200

    firstt

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 export default function ProtectedRoute(props) {
-  if (props.userData == null) {
+  if (props.userData !== null) {
     return <Navigate to="/login" />;
   } else {
     console.log(props.userData); 
