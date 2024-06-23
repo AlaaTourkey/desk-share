b32b99e9f1bab59f62cbe43866cdcd0774f3823f
AlaaTourkey <alaatourkey@gmail.com>
2024-06-23 05:07:40 +0300

    animate

--- src/components/Dashboard/AllWorkspaces.jsx
+++ src/components/Dashboard/AllWorkspaces.jsx
@@ -41,7 +41,7 @@ function AllWorkspaces() {
           {wSpace.map((workspace) => (
             <div className="col-6 col-md-3 text-center text-capitalize mb-4" key={workspace._id}>
               <div className="card h-100" style={{ width: '16rem' }}>
-                <img src={workspace.cover} alt="Workspace Cover" className="card-img-top rounded-3 w-100 h-100" />
+                <img src={workspace.cover || "/assets/dashboard/cover.png"} alt="Workspace Cover" className="card-img-top rounded-3 w-100 h-100" />
                 <div className="card-body">
                   <h5 className="card-title" style={{ color: "#62939f" }}>{workspace.name}</h5>
                   <h6 className="card-text text-dark-emphasis">City: {workspace.city.name}</h6>
